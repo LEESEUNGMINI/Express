0313cfe1465a0f937e50b2988fdf301ed470cb2c
LEESEUNGMINI <dltmdals7295@naver.com>
2024-02-13 10:22:57 +0900

    first

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hi,, broou..!");
+});
+
+app.listen(PORT, () => {
+  console.log(`♥Sever on: http://localhost:${PORT}`);
+});
